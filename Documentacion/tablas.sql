dd4d4c74172a6c724af7c91231626b5a06224a53
israelpalma <ipalm001@gmail.com>
2020-06-17 22:10:43 -0400

    modificacion script

--- Documentacion/tablas.sql
+++ Documentacion/tablas.sql
@@ -1,16 +1,11 @@
-create table alumnos
-(
-    id       int auto_increment
-        primary key,
-    nombre   text          not null,
-    apellido text          not null,
-    edad     int default 1 null
+create table alumnos (
+    id int AUTO_INCREMENT increment primary key,
+    nombre text not null,
+    apellido text not null,
+    edad int default 1 null
 );
-
-create table cursos
-(
-    id     int auto_increment
-        primary key,
+create table cursos (
+    id int AUTO_INCREMENT primary key,
     nombre text not null,
     codigo text not null
 );
